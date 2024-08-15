9243e90d64a936cc36191bf3e49e4a4bf20320d3
xiao_Q <mckekejun@163.com>
2024-08-15 17:01:15 +0800

    Update and add

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -128,6 +128,12 @@ export default defineConfig({
         }, 
         link: 'https://space.bilibili.com/438125945' 
       },
+      {
+        icon: {
+         svg:'<svg t="1723712354637" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5474" width="20" height="20"><path d="M512 504L764.8 68.8h-134.4L512 275.2 393.6 70.4h-134.4L512 504zM819.2 70.4L512 600 204.8 68.8H0l512 886.4L1024 70.4H819.2z" fill="#888888" p-id="5475"></path></svg>',
+        }, 
+        link:'https://vitepress.dev/zh/'
+      },
     ],
     footer:{
       copyright: "Copyright © 2024 爱吃饭的小Q. All rights reserved",
