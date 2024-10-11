e1b6c4581d64796a6c35decfd3df0c4803b215ff
MajorFoxs_OvO <112736920+Re0XIAOPA@users.noreply.github.com>
2024-10-11 09:37:25 +0800

    update

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,6 +2,10 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 /** @type {import('vitepress').Theme} */
 export default {
@@ -11,7 +15,16 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // 注册全局组件，如果你不想使用也可以不添加
+    ctx.app.component('vImageViewer', vImageViewer);
     // ...
-  }
+},
+setup() {
+    // 获取路由
+    const route = useRoute();
+    // 使用
+    imageViewer(route);
+}
 }
