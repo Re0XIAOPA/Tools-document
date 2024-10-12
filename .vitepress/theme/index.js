1cf16e477b31a47b0ab5abe41e779229962ce3d2
MajorFoxs_OvO <112736920+Re0XIAOPA@users.noreply.github.com>
2024-10-12 17:32:24 +0800

    更新 index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -20,11 +20,11 @@ export default {
     // 注册全局组件，如果你不想使用也可以不添加
     ctx.app.component('vImageViewer', vImageViewer);
     // ...
-},
-setup() {
+  },
+  setup() {
     // 获取路由
     const route = useRoute();
     // 使用
     imageViewer(route);
-}
+  }
 }
