f711d024b08783a1e5f36bfa6d21021ac603bedb
MajorFoxs_OvO <112736920+Re0XIAOPA@users.noreply.github.com>
2024-10-16 01:18:14 +0800

    更新 config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -264,7 +264,7 @@ export default defineConfig({
       },
     ],
     footer: {
-      copyright: 'Copyright © 2024 <a href="https://github.com/your_username" target="_blank" style="text-decoration: none;">爱吃饭的小Q</a>. All rights reserved',
+      copyright: 'Copyright © 2024 <a href="https://github.com/Re0XIAOPA" target="_blank" style="text-decoration: none;">爱吃饭的小Q</a>. All rights reserved',
     },
 
   },
