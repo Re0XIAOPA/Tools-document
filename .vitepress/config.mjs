b1fd98a0c62159b641ac684bdec8b2497fbc0be4
MajorFoxs_OvO <112736920+Re0XIAOPA@users.noreply.github.com>
2024-10-20 02:46:11 +0800

    更新 config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -64,14 +64,13 @@ export default defineConfig({
               link: "/zh/Git/git-code"
             },
             {
-              text: 'MySQL的安装和环境配置',
-              link: '/zh/MySQL/MySQL-install'
-            },
-            {
               text: 'Vitepress搭建',
               link: '/zh/OS/vitepress-install'
             },
-
+            {
+              text: 'MySQL的安装和环境配置',
+              link: '/zh/MySQL/MySQL-install'
+            },
           ]
         },
         {
